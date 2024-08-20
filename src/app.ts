dcef73882d7a188f0b710a1da49aa22a1eb8723f
LuLuMS0206 <luciamacho00@gmail.com>
2024-08-20 15:35:14 +0200

    add chnage port

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
+
+
 import express, { NextFunction, Request, Response } from 'express';
 import bookingController from './controllers/bookingControllers';
 import 'dotenv/config';
 import roomsController from './controllers/roomController';
 import userController from './controllers/userContoller';
-import contactController from './controllers/contactController'
-import loginController from './controllers/loginController'
+import contactController from './controllers/contactController';
+import loginController from './controllers/loginController';
 import { authenticateTokenMiddleware } from './middleware/auth';
 import mongoose from 'mongoose';
+import cors from 'cors'; 
 
 const start = async () => {
   try {
     await mongoose.connect(
       "mongodb+srv://luciamacho00:wtNqhbB03R7ZFY2w@cluster0.qavfymp.mongodb.net/mirandaMongo"
-    )
+    );
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -23,15 +26,20 @@ start();
 process.env.TOKEN_SECRET;
 
 export const app = express();
+
+app.use(cors()); 
+
 app.use(express.json());
-app.use('/', (_req, res)=>{
-  return res.send('Lucia')
+
+app.use('/', (_req, res) => {
+  return res.send('Lucia');
 });
+
 app.use('/auth', loginController);
-app.use('/bookings',authenticateTokenMiddleware, bookingController);
-app.use('/rooms',authenticateTokenMiddleware, roomsController);
-app.use('/users',authenticateTokenMiddleware, userController);
-app.use('/contacts',authenticateTokenMiddleware, contactController);
+app.use('/bookings', authenticateTokenMiddleware, bookingController);
+app.use('/rooms', authenticateTokenMiddleware, roomsController);
+app.use('/users', authenticateTokenMiddleware, userController);
+app.use('/contacts', authenticateTokenMiddleware, contactController);
 
 app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err.stack);
